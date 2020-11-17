bfc920998b74f2de719c5d09d08e69a0447aa8b0
grace00821 <71898013+grace00821@users.noreply.github.com>
2020-11-17 19:01:01 +0900

    Update load_name.cpp

--- load_name.cpp
+++ load_name.cpp
@@ -2,15 +2,15 @@
 #include <ifstream>
 using namespace std;
 
-void load_name(string* user){
+void load_name(string* user, int* num){
         ifstream fin;
 	string line;
 	fin.open("game_users.txt");
 	if(fin.fail()){
 		exit(1);
 	}
-	for(int i=0; i<MAXUSERS; ++i){
-		getline(fin, line);
+	while(getline(fin, line)){
+		++num;
 		int index = line.find(" ");
 		user[i] = line.substr(0, index);
 	}
