29a203c20ebb874f7d4692fd66bbb5fa1e92b877
grace00821 <71898013+grace00821@users.noreply.github.com>
2020-11-17 18:37:49 +0900

    Create load_name.cpp

---
+++ load_name.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <ifstream>
+using namespace std;
+
+void load_name(string* user){
+        ifstream fin;
+	string line;
+	fin.open("game_users.txt");
+	if(fin.fail()){
+		exit(1);
+	}
+	for(int i=0; i<MAXUSERS; ++i){
+		getline(fin, line);
+		int index = line.find(" ");
+		user[i] = line.substr(0, index);
+	}
+}
