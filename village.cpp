462231dccb25f96baff7ea40797b74f2fa8af163
grace00821 <71898013+grace00821@users.noreply.github.com>
2020-11-18 01:48:22 +0900

    Update village.cpp

--- village.cpp
+++ village.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
+#include <fstream>
 #include "village.h"
 using namespace std;
 
 void village(Game_data* data, int* location){
+	ofstream fout;
 	int sel = 0;
-	cout << "Village : [1] Go to store [2] Go to dungeon [3] Heal HP" << endl;
+	cout << "Village : [1] Go to store [2] Go to dungeon [3] Heal HP [4] Save" << endl;
 	cin >> sel;
 	if(sel == 1){location = 2;}
 	else if(sel == 2){location = 3;}
 	else if(sel == 3){
 		hp = totalHP;
 		cout << "Healed... [HP] " << hp << " / " << totalHP << endl;
+	}
+	else if(sel == 4){
 	}
 }
