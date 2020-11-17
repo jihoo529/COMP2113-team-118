00cfa8abf1b27f24f504f049d00e0aa610a9d343
grace00821 <71898013+grace00821@users.noreply.github.com>
2020-11-17 18:19:41 +0900

    Update load_data.cpp

--- load_data.cpp
+++ load_data.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include <string>
+#include "load_data.h"
+using namespace std;
 
+void load_data(string* username, int* level, int* max_xp, int* current_xp, int* hp, int* ad, int* bp, int* cp, int* debuff){
+	ifstream fin;
+	string line;
+	fin.open("game_users.txt");
+	if(fin.fail()){
+		exit(1);
+	}
+	for(int i=0; i<MAXUSERS; ++i){
+		getline(fin, line);
+		int index = line.find(" ");
+		username[i] = line.substr(0, index);
+		line = line.erase(0, index);
+		index = line.find(" ");
+		level[i] = line.substr(0, index);
+		line = line.erase(0, index);
+		index = line.find(" ");
+		max_xp[i] = line.substr(0, index);
+		line = line.erase(0, index);
+		index = line.find(" ");
+		current_xp[i] = line.substr(0, index);
+		line = line.erase(0, index);
+		index = line.find(" ");
+		hp[i] = line.substr(0, index);
+		line = line.erase(0, index);
+		index = line.find(" ");
+		ad[i] = line.substr(0, index);
+		line = line.erase(0, index);
+		index = line.find(" ");
+		bp[i] = line.substr(0, index);
+		line = line.erase(0, index);
+		index = line.find(" ");
+		cp[i] = line.substr(0, index);
+		line = line.erase(0 , index);
+		debuff[i] = line;
+	}
+}
