66127cdb31350c341b000bd391199db22f3e07de
grace00821 <71898013+grace00821@users.noreply.github.com>
2020-11-18 00:27:29 +0900

    Create new_user.cpp

---
+++ new_user.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void new_user(Usernames* user, Game_data* data, New_player initial){
+	bool run = 1;
+	while(run == 1){
+		int count = 0;
+		for(int i = 0; i<user.num; ++i){
+			if(user.user[i] == newname){
+				cout << "This username is unavailable." << endl;
+			}
+			else{++count;}
+		}
+		if(user.num == count){
+			New_player initial;
+			user.user[user.num] = newname;
+			data.username = user.user[user.num];
+			data.play.level = initial.level;
+			data.play.max_xp = initial.max_xp;
+			data.play.current_xp = initial.current_xp;
+			data.play.hp = initial.hp;
+			data.play.ad = initial.ad;
+			data.play.bp = initial.bp;
+			data.play.cp = initial.cp;
+			current_player = user.num;
+			++user.num;
+			run = 0;
+		}
+	}
+}
