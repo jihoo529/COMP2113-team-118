6d4120c783977a3bfffdcdef6e160521b4a3617b
grace00821 <71898013+grace00821@users.noreply.github.com>
2020-11-18 00:27:49 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -48,31 +48,7 @@ int main(){
 	string newname;
 	cin >> newname;
 	if(sel == 1){
-		bool run = 1;
-		while(run == 1){
-			int count = 0;
-			for(int i = 0; i<user.num; ++i){
-				if(user.user[i] == newname){
-					cout << "This username is unavailable." << endl;
-				}
-				else{++count;}
-			}
-			if(user.num == count){
-				New_player initial;
-				user.user[user.num] = newname;
-				data.username = user.user[user.num];
-				data.play.level = initial.level;
-				data.play.max_xp = initial.max_xp;
-				data.play.current_xp = initial.current_xp;
-				data.play.hp = initial.hp;
-				data.play.ad = initial.ad;
-				data.play.bp = initial.bp;
-				data.play.cp = initial.cp;
-				current_player = user.num;
-				++user.num;
-				run = 0;
-			}
-		}
+		new_user(&user, &data, initial);
 		tutorial();
 	}
 	else if(sel == 2){
