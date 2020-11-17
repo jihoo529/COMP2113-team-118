0c6b61ab5993fac420d278e20fe8130725ec0220
grace00821 <71898013+grace00821@users.noreply.github.com>
2020-11-18 00:39:43 +0900

    Update new_user.h

--- new_user.h
+++ new_user.h
@@ -1,6 +1,6 @@
 #ifndef NEW_USER_H
 #define NEW_USER_H
 
-void new_user(Usernames* user, Game_data* data, New_player initial);
+void new_user(Usernames* user, Game_data* data, New_player initial, string newname);
 
 #endif
