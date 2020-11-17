0b06538171cc42544601dea90ac035b1700e4424
grace00821 <71898013+grace00821@users.noreply.github.com>
2020-11-17 23:59:53 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,17 @@
 #include <string>
 #include <fstream>
 #include "load_data.h"
+#include "load_name.h"
 using namespace std;
-const int MAXHP = 10000;
-const int MAXAD = 10000;
-const int MAXBP = 100;
-const int MAXCP = 100;
-const int MAXLEVEL = 100;
-const int MAXUSERS = 5;
+
+const MAXHP = 10000;
+const MAXAD = 10000;
+const MAXBP = 40;
+const MAXLEVEL = 100;
+const MAXUSERS = 5;
+
 struct Player{
-	int level, max_xp, current_XP, HP, AD, BP, CP; // debuff; removed debuff
+	int level, max_xp, current_xp, hp, ad, bp, cp;
 };
 struct Game_data{
 	string username;
@@ -20,50 +22,67 @@ struct New_player{
 	int level = 1;
 	int max_xp = level*50;
 	int current_xp = 0;
-	int HP = 50, AD = 10;//, BP = 1;
-	int BP = 10;
-	int CP = 0;
+	int hp = 50, ad = 10, bp = 1;
+	int cp = 0, debuff = 0;
 };
 struct Usernames{
 	string user[MAXUSERS];
+	int num = 0;
 }
 
 void save_data();
-string check_name();
-string load_name();
+void tutorial();
 
 int main(){
 	ofstream fout;
 	ifstream fin;
 	Usernames user;
 	Game_data data;
+	int current_player = -1;
 	cout << "Welcome to Dungeon" << endl;
 	cout << "[1] New Player [2] Continue" << endl;
-	load_name(*user.user[0]);
+	load_name(*user.user[0], *user.num);
 	int sel = 0;
 	cin >> sel;
+	cout << "Username : " << endl;
+	string newname;
+	cin >> newname;
 	if(sel == 1){
 		bool run = 1;
 		while(run == 1){
 			int count = 0;
-			cout << "Username : " << endl;
-			string username;
-			cin >> username;
-			for(int i = 0; i<5; ++i){
-				if(username == names.username[i]){
-					cout << "Username already exists" << endl;
-					break;
+			for(int i = 0; i<user.num; ++i){
+				if(user.user[i] == newname){
+					cout << "This username is unavailable." << endl;
 				}
 				else{++count;}
 			}
-			if(count == 5){
+			if(user.num == count){
+				New_player initial;
+				user.user[user.num] = newname;
+				data.username = user.user[user.num];
+				data.play.level = initial.level;
+				data.play.max_xp = initial.max_xp;
+				data.play.current_xp = initial.current_xp;
+				data.play.hp = initial.hp;
+				data.play.ad = initial.ad;
+				data.play.bp = initial.bp;
+				data.play.cp = initial.cp;
+				data.play.debuff = initial.debuff;
+				current_player = user.num;
+				++user.num;
+				run = 0;
+			}
+		}
+		tutorial();
+	}
+	else if(sel == 2){
+		for(int i = 0; i<user.num; ++i){
+			if(user.user[i] == newname){
+				current_player = i;
 				break;
 			}
 		}
+		load_data(current_player, &data.username, &data.play.level, &data.play.max_xp, &data.play.current_xp, &data.play.hp, &data.play.ad, &data.play.bp, &data.play.cp);
 	}
-	else if(sel == 2){
-		check_name(*user.);
-	}
-	load_data(&data.username, &data.play.level, &data.play.max_xp, &data.play.current_xp, &data.play.hp, &data.play.ad, &data.play.bp, &data.play.cp, &data.play.debuff);
-
 }
