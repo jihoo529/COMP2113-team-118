5ad65a7ee49e2aa1f2dff5341d86a87c9bf59c7a
grace00821 <71898013+grace00821@users.noreply.github.com>
2020-11-18 01:43:35 +0900

    Update village.cpp

--- village.cpp
+++ village.cpp
@@ -5,7 +5,11 @@ using namespace std;
 void village(Game_data* data, int* location){
 	int sel = 0;
 	cout << "Village : [1] Go to store [2] Go to dungeon [3] Heal HP" << endl;
+	cin >> sel;
 	if(sel == 1){location = 2;}
 	else if(sel == 2){location = 3;}
-	else if(sel == 3){}
+	else if(sel == 3){
+		hp = totalHP;
+		cout << "Healed... [HP] " << hp << " / " << totalHP << endl;
+	}
 }
