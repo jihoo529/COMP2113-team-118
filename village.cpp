fc9e8f0a61d0ee76fbec56ceeac0eb80111dab53
grace00821 <71898013+grace00821@users.noreply.github.com>
2020-11-18 23:47:51 +0900

    Update village.cpp

--- village.cpp
+++ village.cpp
@@ -6,6 +6,7 @@ using namespace std;
 void village(Game_data* data, int* location){
 	ofstream fout;
 	int sel = 0;
+	cout << "Welcome to village" << endl;
 	cout << "Village : [1] Go to store [2] Go to dungeon [3] Heal HP [4] Save" << endl;
 	cin >> sel;
 	if(sel == 1){location = 2;}
