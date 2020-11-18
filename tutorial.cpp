efe2e4f874f971c8f2ad55db3da0cfce409f3dae
jihoo529 <jhlee2@LAPTOP-4IGDD36D.localdomain>
2020-11-19 01:28:31 +0900

    tutorial updated

--- tutorial.cpp
+++ tutorial.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <unistd.h>
 using namespace std;
 
 void tutorial(){
+	int run = 1;
+	while(run == 1){
+		system("clear");
+		cout << "Welcome to our village !" << endl;
+		sleep(1.5);
+		cout << "You can either travel dungeon or visit store in this village" << endl;
+		sleep(1.5);
+		cout << "You can buy health potions or upgrade your weapon in the store" << endl;
+		cout << "If you upgrade your weapon, you will give more critical damage to enemy." << endl;
+		sleep(1.5);
+		cout << "You can level up if you defeat your enemy. Your abilities will then be enhanced" << endl;
+		cout << "But if you lose, you have to get back to village. Enemy will take some gold from you" << endl;
+		sleep(1.5);
+		cout << "Then..... GOOD LUCK !!!" << endl;
+		cout << endl;
+		cout << "If you want to go through again, please enter 1. Enter 2 other wise" << endl;
+		int input;
+		cin >> input;
+		if(input == 1){
+			continue;
+		}else if(input == 2){
+			break;
+		}
+	}
 	
 }
