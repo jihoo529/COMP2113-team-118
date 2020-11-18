7680178402206ebad23359f23009d480e0a463ee
grace00821 <71898013+grace00821@users.noreply.github.com>
2020-11-19 00:51:32 +0900

    Update save_data.cpp

--- save_data.cpp
+++ save_data.cpp
@@ -1,7 +1,54 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "save_data.h"
 using namespace std;
 
 void save_data(Game_data* data, int* location){
+	ifstream fin;
+	ofstream fout;
+	int total_player=0;
+	int player=0;
+	string line;
+	fin.open("game_memory.txt");
+	if(fin.fail()){
+		cout << "There was a problem in accessing the game memory (1)" << endl;
+		exit(1);
+	}
+	while(getline(fin, line)){
+		int index = line.find(" ");
+		if(data.username == line.substr(0, index)){
+			player = i;
+		}
+		++total_player;
+	}
+	line.erase();
+	string* text = new string[total_player];
+	for(int i = 0; i<total_player; ++i){
+		if(i == player){
+			line = data.username + " ";
+			line = data.play.level + " ";
+			line = data.play.max_xp + " ";
+			line = data.play.current_xp + " ";
+			line = data.play.totalHP + " ";
+			line = data.play.hp + " ";
+			line = data.play.ad + " ";
+			line = data.play.bp + " ";
+			line = data.play.cp + " ";
+			line = data.play.money;
+			text[i] = line;
+			++i;
+		}
+		getline(fin, text[i]);
+	}
+	fin.close();
+	fout.open("game_memory.txt");
+	if(fout.fail()){
+		cout << "There was a problem in accessing the game memory (2)" << endl;
+		exit(1);
+	}
+	for(int i = 0; i<total_player; ++i){
+		fout << text[i] << endl;
+	}
+	fout.close();
 }
