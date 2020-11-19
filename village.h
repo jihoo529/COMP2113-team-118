25258d4cb9cd3b2433403017612a3891a9fa420c
jihoo529 <50173143+jihoo529@users.noreply.github.com>
2020-11-19 23:45:38 +0900

    Update village.h

--- village.h
+++ village.h
@@ -1,6 +1,17 @@
 #ifdef VILLAGE_H
 #define VILLAGE_H
 
-void village(Game_data* data, int* location);
+struct Game_data;
+
+class Village{
+
+private:
+  
+publc:
+  void village(Game_data* data, int* location);
+};
+
+
 
 #endif
+
