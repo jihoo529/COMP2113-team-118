8f0be82b1d2739aad2352585b158262bb152441f
jihoo529 <50173143+jihoo529@users.noreply.github.com>
2020-11-20 02:01:46 +0900

    Update village.h

--- village.h
+++ village.h
@@ -1,7 +1,7 @@
 #ifdef VILLAGE_H
 #define VILLAGE_H
-
-int location;
+#include "common.h"
+/*int location;
 struct Game_data;
 
 class Village{
@@ -11,7 +11,8 @@ private:
 public:
   void village(Game_data* data, int* location);
 };
-
+*/
+void vilalge(Game_data* data, int* location);
 
 
 #endif
