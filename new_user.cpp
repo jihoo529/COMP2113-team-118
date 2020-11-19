add35c95c823b7e7c2477496cda24a17ffca80f3
jihoo529 <50173143+jihoo529@users.noreply.github.com>
2020-11-20 01:02:46 +0900

    Update new_user.cpp

--- new_user.cpp
+++ new_user.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <string>
 #include "new_user.h"
+#include "common.h"
 using namespace std;
+
+Usernames user;
+Game_data data;
+New_player initial;
 
 void new_user(Usernames* user, Game_data* data, New_player initial){
 	cout << "Username : " << endl;
@@ -10,25 +15,25 @@ void new_user(Usernames* user, Game_data* data, New_player initial){
 	bool run = 1;
 	while(run == 1){
 		int count = 0;
-		for(int i = 0; i<user.num; ++i){
-			if(user.user[i] == newname){
+		for(int i = 0; i<user->num; ++i){
+			if(user->user[i] == newname){
 				cout << "This username is unavailable." << endl;
 			}
 			else{++count;}
 		}
-		if(user.num == count){
-			user.user[user.num] = newname;
-			data.username = user.user[user.num];
-			data.play.level = initial.level;
-			data.play.max_xp = initial.max_xp;
-			data.play.current_xp = initial.current_xp;
-			data.play.hp = initial.hp;
-			data.play.ad = initial.ad;
-			data.play.bp = initial.bp;
-			data.play.cp = initial.cp;
-			data.play.money = initial.money;
-			current_player = user.num;
-			++user.num;
+		if(user->num == count){
+			user->user[user->num] = newname;
+			data->username = user->user[user->num];
+			data->play.level = initial.level;
+			data->play.max_xp = initial.max_xp;
+			data->play.current_xp = initial.current_xp;
+			data->play.hp = initial.hp;
+			data->play.ad = initial.ad;
+			data->play.bp = initial.bp;
+			data->play.cp = initial.cp;
+			data->play.money = initial.money;
+			current_player = user->num;
+			++user->num;
 			run = 0;
 		}
 	}
