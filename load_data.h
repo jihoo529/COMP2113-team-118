68f4bc80166c42b1b9921df54da531b137e97d67
jihoo529 <50173143+jihoo529@users.noreply.github.com>
2020-11-20 01:13:51 +0900

    Update load_data.h

--- load_data.h
+++ load_data.h
@@ -1,6 +1,8 @@
 #ifndef LOAD_DATA_H
 #define LOAD_DATA_H
 
+#include "common.h"
+
 void load_data(int current_player, string* username, int* level, int* max_xp, int* current_xp, int* hp, int* ad, int* bp, int* cp);
 
 #endif
