f2a469e64f87d1a2cc745c7ead52064ecb31db5e
jihoo529 <50173143+jihoo529@users.noreply.github.com>
2020-11-20 02:06:37 +0900

    Update new_user.h

--- new_user.h
+++ new_user.h
@@ -3,6 +3,6 @@
 
 #include "common.h"
 
-void new_user(Usernames* user, Game_data* data, New_player initial);
+void new_user(Usernames* user, Game_data* data, int* current_player, New_player initial);
 
 #endif
