12aeadac19b70fdc3fbe74c8533fdc12d02f8814
jihoo529 <50173143+jihoo529@users.noreply.github.com>
2020-11-19 23:47:28 +0900

    Update save_data.h

--- save_data.h
+++ save_data.h
@@ -1,6 +1,15 @@
 #ifndef SAVE_DATA_H
 #define SAVE_DATA_H
 
-void save_data(Game_data* data, int* location);
+struct Game_data;
 
+class Save_data{
+  
+private:
+
+public:
+  void save_data(Game_data* data, int* location);
+};
+  
 #endif
+
