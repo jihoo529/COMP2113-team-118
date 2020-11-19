1bbd81d6f2abb86c692f8d8d794770fe6afa4f4a
jihoo529 <50173143+jihoo529@users.noreply.github.com>
2020-11-20 01:14:27 +0900

    Update load_data.h

--- load_data.h
+++ load_data.h
@@ -1,8 +1,9 @@
 #ifndef LOAD_DATA_H
 #define LOAD_DATA_H
+#include <string>
 
 #include "common.h"
 
-void load_data(int current_player, string* username, int* level, int* max_xp, int* current_xp, int* hp, int* ad, int* bp, int* cp);
+void load_data(int current_player, std::string* username, int* level, int* max_xp, int* current_xp, int* hp, int* ad, int* bp, int* cp);
 
 #endif
