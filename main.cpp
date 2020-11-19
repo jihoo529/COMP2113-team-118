78714a67b992e072ad16f94b30df8b888a147c26
jihoo529 <50173143+jihoo529@users.noreply.github.com>
2020-11-20 02:19:02 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -106,8 +106,8 @@ int main(){
 	}
 	int location = 1;
 	while(location != 0){
-		if(location == 1){village(&data, &location);}
-		else if(location == 2){store(&data, &location);}
+		//if(location == 1){village(&data, &location);}
+		if(location == 2){store(&data, &location);}
 		else if(location == 3){dungeon(&data, &location);}
 	}
 	cout << "Game quit... See you next time!" << endl;
