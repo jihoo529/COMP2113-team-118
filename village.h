489f6857e55c6771c5211c202104e41af50ae772
jihoo529 <50173143+jihoo529@users.noreply.github.com>
2020-11-20 02:17:27 +0900

    Update village.h

--- village.h
+++ village.h
@@ -2,18 +2,8 @@
 #define VILLAGE_H
 #include "save_data.h"
 #include "common.h"
-/*int location;
-struct Game_data;
 
-class Village{
-
-private:
-  
-public:
-  void village(Game_data* data, int* location);
-};
-*/
-void village(Game_data* data, int* location);
+void vilage(Game_data* data, int* location);
 
 
 #endif
