0ecf01cb2611f25f58cfe01ec02897477853f5ba
jihoo529 <50173143+jihoo529@users.noreply.github.com>
2020-11-20 15:17:08 +0900

    Update village.cpp

--- village.cpp
+++ village.cpp
@@ -16,7 +16,14 @@ void village(Game_data* data, int* location){
 	ofstream fout;
 	int sel = 0;
 	cout << "Welcome to village" << endl;
+	cout << "[LV] " << data->play.level << endl;
+	cout << "[XP] " << data->play.current_xp << "/" << data->play.max_xp << endl;
+	cout << "[HP] " << data->play.hp << "/" << data->play.totalHP << endl;
+	cout << "[AD] " << data->play.ad << endl;
+	cout << "[CP] " << data->play.cp << endl;
+	cout << "-----------------------------------------------------------------" << endl;
 	cout << "Village : [1] Go to store [2] Go to dungeon [3] Heal HP [4] Save" << endl;
+	
 	cin >> sel;
 	if(sel == 1){location[0] = 2;}
 	else if(sel == 2){location[0] = 3;}
