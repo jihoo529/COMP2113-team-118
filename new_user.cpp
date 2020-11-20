4c05bf7d1cc7a64c874d6822f94cc692b005b822
grace00821 <71898013+grace00821@users.noreply.github.com>
2020-11-20 14:37:24 +0900

    Update new_user.cpp

--- new_user.cpp
+++ new_user.cpp
@@ -22,7 +22,8 @@ void new_user(Usernames* user, Game_data* data, int* current_player, New_player 
 			}
 			else{++count;}
 		}//end of for loop 1
-		if((user->num) == count+1){
+		cout << user->num << " " << count;
+		if(user->num == count){
 			string* temp = new string[user->num +1];
 			for(int i = 0; i<user->num; ++i){
 				temp[i] = user->user[i];
