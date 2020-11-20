e77885fbff5298d541ab23b57133afdfe89f8f27
grace00821 <71898013+grace00821@users.noreply.github.com>
2020-11-20 22:20:20 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -130,7 +130,7 @@ int main(){
 		if(data.play.level > 100){data.play.level = 100;}
 		if(data.play.nump < 0){data.play.nump = 0;}
 		
-		if(location == 1){village(&data, &location);}
+		if(location == 1){village(&data, &location, current_player);}
 		else if(location == 2){store(&data, &location);}
 		else if(location == 3){dungeon(&data, &location);}
 	}
