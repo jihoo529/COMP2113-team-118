4d3d3688da84123bd103c7623c7714e5f2da6d3f
grace00821 <71898013+grace00821@users.noreply.github.com>
2020-11-20 20:46:32 +0900

    Update new_user.cpp

--- new_user.cpp
+++ new_user.cpp
@@ -24,14 +24,7 @@ void new_user(Usernames* user, Game_data* data, int* current_player, New_player 
 		}//end of for loop 1
 		//cout << user->num << " " << count;
 		if(user->num == count){
-			string* temp = new string[user->num +1];
-			for(int i = 0; i<user->num; ++i){
-				temp[i] = user->user[i];
-			}
-			temp[user->num] = newname;
-			user->user = temp;
-			delete[] temp;
-			data->username = user->user[user->num];
+			data->username = newname;
 			data->play.level = initial.level;
 			data->play.max_xp = initial.max_xp;
 			data->play.current_xp = initial.current_xp;
