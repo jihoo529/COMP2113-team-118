2143dc05423e14f3545a3199d4f40ad4d6ec6936
grace00821 <71898013+grace00821@users.noreply.github.com>
2020-11-21 17:21:02 +0900

    Update new_user.cpp

--- new_user.cpp
+++ new_user.cpp
@@ -10,6 +10,11 @@ New_player initial;
 int current_player;
 
 void new_user(Usernames* user, Game_data* data, int* current_player, New_player initial){
+	//This function creates a new player by checking the duplication of the usernames and initializing all the player statistics.
+	//The input of this function is the New_player structure and Usernames structure. The New_player structure is used for initializing the new player's statistics by saving the
+	//values into Game_data structure, and the Usernames structure is used for checking the duplication of the new username.
+	//The output of this function is the Game_data structure and current_player integer. The New_player structure values are copied to Game_data structure so the the new player
+	//can begin the game with this statistics. The current_player integer indicates the ID of the user, allowing the program to load and save the data in the correct place.
 	bool run = 1;
 	while(run == 1){
 		cout << "Username : ";
